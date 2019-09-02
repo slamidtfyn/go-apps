38e6c612777bf5a521692686062a91ea514577b3
Søren Larsen <slamidtfyn@gmail.com>
2019-09-02 21:36:56 +0200

    go samples

---
+++ go-variables/app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("GO Variables")
+
+	//float
+	a := 3.14
+	fmt.Println(a)
+
+	//Pi
+	fmt.Println(math.Pi)
+
+	//constant
+	const c = 1 / math.Pi
+	fmt.Println(c)
+}
